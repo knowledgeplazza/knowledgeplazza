39a1184178bcbc16bd7dba5c6a678c333d82ee14
Jackson Smith <rjsmith1999@gmail.com>
2017-01-06 00:07:23 -0500

    Hide size help in production mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-root',
   template: `
-  <span fxShow="false" fxShow.xs> XS </span>
-  <span fxShow="false" fxShow.sm> SM </span>
-  <span fxShow="false" fxShow.md> MD </span>
-  <span fxShow="false" fxShow.lg> LG </span>
-  <span fxShow="false" fxShow.xl> XL </span>
+  <div *ngIf="sizeHelp">
+    <span fxShow="false" fxShow.xs> XS </span>
+    <span fxShow="false" fxShow.sm> SM </span>
+    <span fxShow="false" fxShow.md> MD </span>
+    <span fxShow="false" fxShow.lg> LG </span>
+    <span fxShow="false" fxShow.xl> XL </span>
+  </div>
   <router-outlet></router-outlet>`,
 })
 export class AppComponent {
   constructor() { }
+
+  get sizeHelp(): boolean {
+    return environment.production === false;
+  }
 }
