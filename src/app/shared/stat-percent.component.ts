85dea5dabab2e8df4d5b1cb49194c340766ca47e
Jackson Smith <rjsmith1999@gmail.com>
2017-01-10 13:12:13 -0500

    Clean up stats

---
+++ src/app/shared/stat-percent.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-percent',
+  template:
+  '<div [ngClass]="class" style="padding: 5px"> \
+  {{ stat?.correct / stat?.answeredCount | percent }} \
+  </div>',
+})
+export class StatPercentComponent implements OnInit {
+  @Input() stat: any;
+  @Input() class: string;
+  constructor() { }
+
+  ngOnInit() { }
+}
