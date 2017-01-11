aee821666fdbd3f02522607616a4d47f65f981d3
Jackson Smith <rjsmith1999@gmail.com>
2017-01-10 20:55:10 -0500

    Fix stat display

--- src/app/shared/stat-percent.component.ts
+++ src/app/shared/stat-percent.component.ts
@@ -3,13 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-percent',
   template:
-  '<div *ngIf="stat?.correct !== undefined" [ngClass]="class" style="padding: 5px"> \
+  '<div *ngIf="stat?.answeredCount" [ngClass]="class" style="padding: 5px"> \
   {{ stat?.correct / stat?.answeredCount | percent }} \
   </div>',
 })
 export class StatPercentComponent implements OnInit {
   @Input() stat: any;
   @Input() class: string;
+
   constructor() { }
 
   ngOnInit() { }
